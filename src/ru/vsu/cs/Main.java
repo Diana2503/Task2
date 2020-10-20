296d797800c354ad1d0e07d2a3f8da92b01d5a7f
Diana Besxmelnicyna <dbesxmelnicyna@mail.ru>
2020-10-21 00:15:15 +0300

    Task2

---
+++ src/ru/vsu/cs/Main.java
@@ -0,0 +1,78 @@
+package ru.vsu.cs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = readNumber();
+        
+        if (performedComparisonNumberSegment(x)) {
+            findFigureNumber(x);
+        }
+        else{
+            printAdverseAnswer();
+        }
+    }
+
+    public static void findFigureNumber(int num) {
+        int max = 0;
+        int min = 9;
+
+        while (true) {
+            int a = findRestNumber(num);
+
+            if(max < a) {
+                max = a;
+            }
+
+            if(min > a) {
+                min = a;
+            }
+
+            findNumberAfterDivision(num);
+
+            if(num == 0) {
+                break;
+            }
+        }
+
+        printValueMax(max);
+        printValueMin(min);
+    }
+
+    public static int readNumber() {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Input number from -1000 to 1000: ");
+        return scn.nextInt();
+    }
+
+    public static void printValueMax(int max) {
+        System.out.println("max = " + max);
+    }
+
+    public static void printValueMin(int min) {
+        System.out.println("max = " + min);
+    }
+
+    public static void printAdverseAnswer() {
+        System.out.println("Number isn`t included in the interval");
+    }
+
+    public static boolean performedComparisonNumberSegment(int x) {
+        return (x >= -1000 && x <= 1000);
+    }
+
+    public static int findRestNumber(int num) {
+        return Math.abs(num %10);
+    }
+
+    public static int findNumberAfterDivision(int num) {
+        return num = num / 10;
+    }
+
+}
+
+
+
